ce83dec920c2120d4acadf39c26247be0f6c4c3f
camriemaxon <camriemaxon@yahoo.com>
2019-02-23 14:13:39 -0800

    first day of work

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <title>Home Brew</title>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
